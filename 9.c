340069d972dde3732bc43de96a9953821c7c08b5
bachwer <110678064+bachwer@users.noreply.github.com>
2024-12-03 14:10:30 +0700

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+int main(){
+    int col, row;
+    printf("Enter col and row number: ");
+    scanf("%d", &col);
+    row = col;
+
+    int temp;
+    int num[col][row];
+    int k = 0;
+    int num1[col];
+
+    for(int i = 0; i < col; i++){
+        for(int j = 0; j < row; j++){
+            printf("col %d : row %d: ", i + 1, j +1);
+           scanf("%d", &num[i][j]);
+            if(i == j){
+            num1[k] = num[i][j];
+            k++;
+            }
+        }
+    }
+    int n = sizeof(num1) / sizeof(int);
+    for(int i = 1; i < n ; i++) {
+        for(int j = 0; j < i; j++) {
+            if(num1[i] < num1[j]) {
+                int temp = num1[i];
+                num1[i] = num1[j];
+                num1[j] = temp;
+            }
+        }
+    }
+   printf("Pha tu ma tran chua sap xep: ");
+   printf("\n");
+     for(int i = 0; i < col; i++) {
+        for(int j = 0; j < row; j++) {
+            if(i == j){
+                printf("%d ", num[i][j]);
+                k++;
+            }else {
+                printf("   ");
+            }
+
+        }
+        printf("\n");
+    }
+   
+   
+   
+      printf("Pha tu ma tran cda sap xep: ");
+   
+        printf("\n");
+   k = 0;
+    for(int i = 0; i < col; i++) {
+        for(int j = 0; j < row; j++) {
+            if(i == j){
+                printf("%d ", num1[k]);
+                k++;
+            }else {
+                printf("   ");
+            }
+
+        }
+        printf("\n");
+    }
+
+    
+
+    return 0;
+}
